07da93284f6ea006f6a66ea8a95de7b2a81ed87e
56989_Ayushi <ayushi.d.999@gmail.com>
2023-12-27 14:30:01 +0530

    first commit

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UBER_PROJECT.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VEHICLE_PROJECT.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- uber/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UberConfig(AppConfig):
-    name = 'uber'
